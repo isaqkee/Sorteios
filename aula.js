e06046b51f9de3840df9266e650ee368707c757c
isaqkee <isaquecorreia34@gmail.com>
2023-09-04 16:48:08 -0300

    Finalizado
    finalizado

---
+++ aula.js
@@ -0,0 +1,17 @@
+// Math.random gera um número aleatório
+console.log(Math.random());
+
+// Gerando um número aleatório entre 0 e 20
+console.log(Math.random() * 20);
+console.log(Math.round(Math.random()*5));
+// Math.round arrendonda para inteiro mais próximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Math.ceil arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
